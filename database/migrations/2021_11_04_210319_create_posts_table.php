f611fda5f33fc61bc0a62da1ced59ce518e687c4
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-11-04 23:01:17 +0000

    Added the database relationships and created the user table seeder

--- database/migrations/2021_11_04_210319_create_posts_table.php
+++ database/migrations/2021_11_04_210319_create_posts_table.php
@@ -15,7 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('contents');
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
