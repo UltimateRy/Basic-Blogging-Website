e93a00e5ad11d4809b0fbadd25042219d771dced
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-12-13 19:25:49 +0000

    Removed the redundant CheckOwn middleware reference in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
-        'checkOwn' => \App\Http\Middleware\CheckIfOwns::class,
     ];
 }
