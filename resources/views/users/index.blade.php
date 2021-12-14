3cefa9338d50a038f472f4c688985c2813664ed5
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-12-14 00:08:24 +0000

    Added the vue.js return for the list of users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,14 +7,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Users</title>
+        <title>Profiles</title>
 </head>
 
 <body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
-    <h1>Users</h1>
+    <h1>Profiles</h1>
     <div id="root">
-        <p>@{{ message }}</p>
+        <ul>
+            <li v-for="user in users">@{{ user.name }} </li>
+        </ul>   
     </div>
 
     <ul>
@@ -36,7 +39,16 @@
     var app = new Vue({
         el: "#root",
         data: {
-            message: "It works",
+            users: [],
+        },
+        mounted() {
+            axios.get("{{ route ('api.profiles.index') }}")
+            .then( response => {
+                this.users = response.data;
+            })
+            .catch(response => {
+                console.log(response);
+            })
         },
     });
 </script>
