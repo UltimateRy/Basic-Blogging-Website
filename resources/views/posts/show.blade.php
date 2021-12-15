68c2fc2f9e0af453292e8b772fc93ffa74023abe
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-12-15 03:15:46 +0000

    Added the tailwind interface for the post page and the My Profile page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,26 +1,72 @@
-@extends('layouts.example')
 
-@section('title', 'Post')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
 
-@section('content')
 
-<ul>
-    <li>Post ID: {{$post->id}} </li>
-    <li>Contents: {{$post->contents}} </li>
-    <li>Post made on: {{$post->created_at}} </li>
-<ul>
+    <div class="pt-12 max-w-6xl mx-auto sm:px-6 lg:px-8 w:full">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <div class="flex flex-row content-evenly">
+                    <div class="w-1/2">
+                        <p>{{$post->title}} </p>
+                    </div>
+                    <div class="w-1/2">
+                        <a class="float-right text-right bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full" 
+                        href="{{ route('profiles.show', ['id' => $post->user->id]) }}">Posted by : {{$post->user->username}}</a>
+                    </div>
+                </div> 
+                <br>
+                
+                <div class="bg-blue overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-blue-100 border-b border-gray-200">
+                        <li>Contents: {{$post->contents}} </li>
+                        <br>
+                    </div>
+                </div>
+                <br>
+                <div class="flex flex-row content-evenly">
 
-@can('update', $post)
+                
+                    @can('update', $post)
+                    <div class="w-1/3">
+                        <form method="POST"
+                            action="{{ route('posts.destroy', ['id' => $post->id]) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full" >Edit Post</button>
+                        </form>
+                    </div>
+                    <br>
+                    <div class="w-1/3">
+                        <form method="POST"
+                            action="{{ route('posts.destroy', ['id' => $post->id]) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" >Delete Post</button>
+                        </form>
+                    </div>
+                    @endcan
+                    <div class="w-1/3">
+                        <p type="submit" class="text-center bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded-full" >Post made on: {{date('d-m-Y', strtotime($post->created_at))}} </p>
+                    </div>
+                </div>      
+            </div>
+        </div>
+    </div>
 
-<form method="POST"
-    action="{{ route('posts.destroy', ['id' => $post->id]) }}">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-</form>
-@endcan
+    <div class="py-6 max-w-6xl mx-auto sm:px-6 lg:px-8 w:full">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
 
-<h1> Post ID: {{$post->id}}'s Comments:</h1>
+                <h1>Comments:</h1>
+            </div>
+        </div>
+    </div>
+
 
 <ul>
     @foreach ($post->comments as $comment)
@@ -34,4 +80,4 @@
     @endforeach
 </ul>
 
-@endsection
+</x-app-layout>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,53 +5,57 @@
         </h2>
     </x-slot>
 
-<ul>
-    <div class="py-12 max-w-10xl mx-auto sm:px-6 lg:px-8 w:full">
+    <div class="py-12 max-w-4xl mx-auto sm:px-6 lg:px-8 w:full">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
-                <p class="text-blue-600 text-3xl font-bold text-center">{{$profile->username}} </p>
+                <p class="text-blue-400 text-3xl font-bold text-center">{{$profile->username}} </p>
+                <br>
+                <div class="flex flex-row">
+                    <div class="w-1/2 px-6">
+                        <div class="bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-gray-100 border-b border-gray-200 text-center">
+                                <p>{{$profile->name}} </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-1/2">
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg text-center">
+                            <div class="p-6 bg-gray-100 border-b border-gray-200">
+                                <p>{{$profile->email}} </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <br>         
             </div>
         </div>
     </div>
-    <div class="flex flex-row">
-        <div class="w-1/2 px-12">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p>{{$profile->name}} </p>
-                </div>
-            </div>
-        </div>
-        <div class="w-1/3">
+
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 w:full" >
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p>{{$profile->email}} </p>
-                </div>
+            <div class="p-6 bg-white border-b border-gray-200">
+            <p class="text-blue-400 text-2xl font-bold text-center"> Posts</p>
+                <br>
+                <ul>
+                    @foreach ($posts as $post)
+                    <div class="max-w-8xl mx-auto sm:px-4 lg:px-4 w:full" onclick="location.href='{{route('posts.show', [ 'id' => $post->id ]) }}';" style="cursor: pointer;">
+                        <div class="bg-blue-200 overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-blue-300 border-b border-gray-200">
+                                <p class="text-white transparent text-2xl font-bold">{{$post->title}}</p>
+                                <br>
+                                <p class="text-m text-white">{{$post->contents}}</p>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    @endforeach
+                    {{$posts->links() }}
+                </ul>
             </div>
         </div>
     </div>
-<ul>
-<br>
-
-<div class="max-w-10xl mx-auto sm:px-6 lg:px-8 w:full" >
-    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200">
-        <p class="text-blue-600 text-3xl font-bold"> {{$profile->username}}'s Posts:</p>
-            <br>
-
-            <ul>
-                @foreach ($posts as $post)
-                    <li><h3>Post ID: <a href="{{route('posts.show', [ 'id' => $post->id ]) }}">{{$post->id}}</a></h3></li>
-                    <li><h4>Post Contents: </h4></li>
-                    <li>{{$post->contents}}</li>
-                    <br>
-                    <br>
-                    <br>
-                @endforeach
-                {{$posts->links() }}
-            </ul>
-        </div>
-    </div>
-</div>
-
-
+    <br>
+    <br>
 </x-app-layout>
