b84f7bfe5b5b44910e5733e2c6d758ce193a5587
UltimateRy <64440937+UltimateRy@users.noreply.github.com>
2021-12-17 02:14:06 +0000

    Made the frontpage route and view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,11 +4,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Post') }}
         </h2>
-
-        
     </x-slot>
-
-
     <div class="pt-12 max-w-6xl mx-auto sm:px-6 lg:px-8 w:full">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\FriendsController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontpage');
 });
 
 //DASHBOARD AND FRIENDS ROUTES
